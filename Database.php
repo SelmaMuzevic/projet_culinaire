3e42d0e9a060eb9ec46a27bcce534209ff22685d
selma <selma@gmail.com>
2017-06-28 16:58:24 +0200

    Mettre les commentaires sur database

--- Database.php
+++ Database.php
@@ -1,13 +1,9 @@
 <?php
 
  /**
- *
  * @author selma
  */
-
-
 /*class Database (les choses qu'on peut mettre dedans){
- * 
  */
 /**
     serialize;
@@ -33,10 +29,25 @@ User :              Evenement :
  */
 /**CRUD : creer, read, update, delete
  * 
- * creer User (argument : instance de User) -> prendre cet instance et le mettre dans le fichier
+ * creer User (argument : instance de User) -> prendre cet instance et 
+ * le mettre dans le fichier
  * creer Post/Evenement( instance de post: $post)
- * lire Evenement/Post ( )-> juste recuperer les fichier et les afficher sous forme de tableau
+ * lire Evenement/Post ( )-> juste recuperer les fichier et les afficher 
+ * sous forme de tableau
  * auth User( $ID, $mdp)
+ * 
+ * DATABASE
+ * 
+ * function connexion($identifiant, $mdp)
+ * if(is_file("user/" .$identifiant. ".txt"))->verifier s'il existe un 
+ * utilisateur avec l'identifiant $identifiant.
+ * 
+ * $user = unserialize(file_get_contents(...))->on recupere l'instance de user 
+ * qu'on avait stocké dans le fichier au moment de l'inscription.
+ * 
+ * if($user->getMdp() == $mdp)-> on compare le mdp stocké dans le fichier 
+ * a celui passé en argument de la function .
+ * 
  */
 include_once './Utilisateur.php';
 
@@ -51,8 +62,7 @@ class Database {
 	    }
 	    //On crée un nouveau fichier pour l'utilisateur
 	    $new_file = fopen("utilisateur/".$utilisateur->getLogin() .".txt", "w");
-            
-	    //On met son mdp encrypté dedans
+           
 	    fwrite($new_file, serialize($utilisateur));
 	    //on ferme le fichier
 	    fclose($new_file);
